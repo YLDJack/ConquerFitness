ba4301df34a33635e5200fbb217b2d60b0fff0e8
YLDJack <779618724@qq.com>
2020-05-27 19:56:41 +0800

    从云端获取身体数据

---
+++ cloudfunctions/addPersonalData/index.js
@@ -0,0 +1,28 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+});
+
+const db = cloud.database();
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+
+  const openId = cloud.getWXContext().OPENID;
+  return await db.collection('PersonalData').add({
+    data: {
+      openId:openId,
+      date:event.date,
+      trainState: event.trainState,
+      weight: event.weight,
+      fat: event.fat,
+      ass: event.ass,
+      leg: event.leg,
+      smallleg: event.smallleg,
+      breast: event.breast,
+      arms: event.arms,
+    }
+  })
+}
---
+++ cloudfunctions/getPersonalData/index.js
@@ -0,0 +1,16 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+});
+
+const db = cloud.database();
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const openId = cloud.getWXContext().OPENID;
+  return await db.collection('PersonalData').where({
+    openId: openId,
+    date: event.date
+  }).get()
+}
